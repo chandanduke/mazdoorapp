345b5135f8297c070383dc26d7e38981adefacb9
chandan kumar <chandankumarbk6@gmail.com>
2021-01-18 16:42:20 +0530

    adding first app

--- src/App.js
+++ src/App.js
@@ -7,14 +7,22 @@ function App() {
         Welcome to Mazdoor App<br/>
         Name ----->Pending Amount <br/>
         Manoj ----> 500<br/>
-        Ramesh ---->     250<br/>
+        Ramesh ----> 250<br/>
         ######------date 15-01-2021------######<br/>
         Mohan-->200<br/>
-        Ramesh-->200
+        Ramesh-->200<br/>
         ######------date 16-01-2021------######<br/>
         Mohan-->200<br/>
         Ramesh-->200<br/>
-        Manoj-->500
+        Manoj-->500<br/>
+        ######------date 17-01-2021------######<br/>
+        Mohan-->200<br/>
+        Ramesh-->200<br/>
+        Manoj-->500<br/>
+        ######------date 18-01-2021------######<br/>
+        Mohan-->200<br/>
+        Ramesh-->200<br/>
+        Manoj-->500<br/>
       </header>
     </div>
   );

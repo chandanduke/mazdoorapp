22573509c7ad7db1a8f2dc338b96caff12b808e4
chandan kumar <chandankumarbk6@gmail.com>
2021-02-06 09:10:56 +0530

    adding first app

--- src/App.js
+++ src/App.js
@@ -96,6 +96,13 @@ function App() {
         ######------date 03-02-2021------######<br/>
         Apreliya-->500<br/>
         Ramesh-->200<br/>
+        ######------date 04-02-2021------######<br/>
+        Apreliya-->500 centring<br/>
+        Ramesh-->200<br/>
+        ######------date 05-02-2021------######<br/>
+        Apreliya-->500 centring<br/>
+        Ramesh-->200centring <br/>
+        Mohan-->200centring <br/>
       </header>
     </div>
   );
